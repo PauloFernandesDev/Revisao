9eb5b69d3b0e94f3d0346bc7b37eb90ac90774c4
Marcio Paulo <marciorocha11@gmail.com>
2025-04-17 14:47:17 -0300

    Revisao quantidade salario minimo

---
+++ Revisao salario.py
@@ -0,0 +1,4 @@
+salarioM = float(input("Informe o valor do Salario minimo: "))
+salarioU = float(input("informe o valor do Salario do usuário: "))
+calc = salarioU / salarioM
+print(f"Salario do Usuario XXXX : {salarioU} / equivalente a {calc :.1f} salario minimo.")
