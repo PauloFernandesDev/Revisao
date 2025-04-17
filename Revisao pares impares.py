a9797e2a08cec9492c8e43bc1d500e8fa0852f21
Marcio Paulo <marciorocha11@gmail.com>
2025-04-17 14:28:17 -0300

    Revisao par ou impar, com condisao de repetir

--- Revisao pares impares.py
+++ Revisao pares impares.py
@@ -1,12 +1,14 @@
-numero = int(input("Digite um numero: "))
-
-if numero %2 == 0:
-        if numero > 0:
-            print(f"{numero} é par e positivo")
-        else:
-            print(f"{numero} é par e negativo")
-else:
-    if numero > 0:
-        print(f"{numero} é impar e positivo")
+resp = "s"
+while resp == "s" and "S":
+    numero = int(input("Digite um numero: "))
+    if numero %2 == 0:
+            if numero > 0:
+                print(f"{numero} é par e positivo")
+            else:
+                print(f"{numero} é par e negativo")
     else:
-        print(f"{numero} é impar e negativo")
+        if numero > 0:
+            print(f"{numero} é impar e positivo")
+        else:
+            print(f"{numero} é impar e negativo")
+    resp = input("Deseja verificar um novo numero [s]Sim / [n]Não: ")
