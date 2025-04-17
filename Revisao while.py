e05e7ad1b083eb7a1e6dd1d092a0533b08f65d4e
Marcio Paulo <marciorocha11@gmail.com>
2025-04-17 16:03:52 -0300

    Revisao while + Revisao numeros pares descrecente

--- Revisao while.py
+++ Revisao while.py
@@ -1,4 +1,5 @@
 x = 1
+j = 1
 while x <= 500:
     if x % 2 != 0:
         print(x, end=" ")
---
+++ numeros pares decrescente.py
@@ -0,0 +1,10 @@
+print("FOR:")
+for x in range(100,1,-1):
+    if x % 2 == 0:
+        print(x)
+print("While")
+x= 100
+while x > 1 :
+    if x % 2 == 0:
+        print(x)
+    x-=1
